dab7f173a32056a4a80abc6357141d7a11a605e2
MilagrosKucharski <milagros.kucharski@correo.ucu.edu.uy>
2022-09-13 17:38:14 -0300

    ejercicios

---
+++ Ejercicio 1/src/Library/Estante.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace SRP
+{
+    public class ShelveBook
+    {
+
+        public string LibrarySector { get ; set; }
+        public string LibraryShelve { get ; set; }
+
+        
+        public ShelveBook(String sector, String shelve)
+        {
+            this.LibrarySector = sector;
+            this.LibraryShelve = shelve;
+        }
+
+
+    }
+}
---
+++ Ejercicio 1/src/Library/Libro.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace SRP
+{
+    public class Book
+    {
+
+        public string Title { get ; }
+        public string Author { get ; }
+        public string Code { get ;  }
+
+
+        public Book(String title, String author, String code)
+        {
+            this.Title = title;
+            this.Author = author;
+            this.Code = code;
+        }
+        
+        /*el metodo para cumplir con el srp deberia estar en otra clase, no se pudo implementar correctamente el program
+        
+        public void ShelveBook(String sector, String shelve)
+        {
+            this.LibrarySector = sector;
+            this.LibraryShelve = shelve;
+        }
+        */
+    }
+}
